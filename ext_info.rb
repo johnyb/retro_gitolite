42c6b382e40ec4eca4d015a33d6b925d377b29c0
Julian Bäume <julian@svg4all.de>
2011-01-25 19:02:21 +0100

    add write permissions to code configuration
    This enables the admin to store more permissions regarding code for
    certain groups in the admin area

--- ext_info.rb
+++ ext_info.rb
@@ -1,1 +1,7 @@
 RetroEM::Views.register_extension('ssh_pubkeys', :user, :options)
+
+RetroAM.permission_map do |map|
+  code = map['code']
+  code.permission :write, :label => N_('Write')
+  code.permission :non_fast_forward, :label => N_('Push non fast-forward')
+end
