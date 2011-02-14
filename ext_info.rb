048b4276ce8651cea93034d131f0175f0cf5d69a
Julian Bäume <julian@svg4all.de>
2011-02-14 16:56:13 +0100

    rename permission
    since it’s not clear, that these permissions work for git repos only,
    rename it to be more general

--- ext_info.rb
+++ ext_info.rb
@@ -3,5 +3,5 @@ RetroEM::Views.register_extension('ssh_pubkeys', :user, :options)
 RetroAM.permission_map do |map|
   code = map['code']
   code.permission :write, :label => N_('Write')
-  code.permission :non_fast_forward, :label => N_('Push non fast-forward')
+  code.permission :admin, :label => N_('Administrate')
 end
